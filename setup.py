8ef970c6f5414ac4bb720f25e69858081b889896
ben <b.talbot@qut.edu.au>
2021-04-08 16:08:38 +1000

    Fix install requirements for Python package

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='acrv_datasets',
-      version='0.1.0',
+      version='0.9.0',
       author='Ben Talbot',
       author_email='b.talbot@qut.edu.au',
       description='Datasets manager for the Best of ACRV',
       long_description=long_description,
       long_description_content_type='text/markdown',
       packages=find_packages(),
-      install_requires=['colorama', 'pyyaml', 'tqdm'],
+      install_requires=['colorama', 'pyyaml', 'requests', 'tqdm'],
       classifiers=(
           "Programming Language :: Python :: 3",
           "License :: OSI Approved :: BSD License",
